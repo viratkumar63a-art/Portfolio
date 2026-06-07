9c338fa98315bb528eb7453df1a762237f032a23
Virat Kumar <viratkumar63a@gmail.com>
2026-06-07 09:23:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+$('.site-main .about-area .owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    dots: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        560: {
+            items: 2
+        }
+    }
+})
